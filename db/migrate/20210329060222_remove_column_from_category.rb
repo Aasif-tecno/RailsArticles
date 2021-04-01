7824b0bcc1b15f3160078533b8b484b394f95718
Mohammed Aasif <mohammedaasif@tecnovators.in>
2021-04-01 10:44:13 +0530

    add autheitication to category and remove the forign key

--- db/migrate/20210329060222_remove_column_from_category.rb
+++ db/migrate/20210329060222_remove_column_from_category.rb
@@ -1,5 +1,5 @@
 class RemoveColumnFromCategory < ActiveRecord::Migration[6.1]
   def change
-    remove_reference :categories, :articles, null: false, foreign_key: true
+    remove_reference :categories, :articles
   end
 end
